28c411c97d549f3a02c5223a3228d5943b816c5a
Evgeen <evgeen@inbox.ru>
2021-02-09 16:18:53 +0300

    Create calculation formula

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+
+  let num = parseFloat(sampleActivity);
+  if (typeof sampleActivity == 'string' && num > 0 && num < 15) {
+    return Math.ceil(Math.log(MODERN_ACTIVITY / num) * (HALF_LIFE_PERIOD / .693));
+  }
+  return false
 };
